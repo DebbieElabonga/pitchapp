89c946598d09625b4cfed24956cc9bc1a67ddc78
DebbieElabonga <debbie.elabonga@student.moringaschool.com>
2021-06-13 11:55:17 +0300

    tests

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from . import db
 from datetime import datetime, timezone, time, timedelta,timezone
-
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__='users'
@@ -13,6 +13,17 @@ class User(db.Model):
     pitch=db.relationship('Pitch',backref='user',lazy='dynamic')
     pass_secure = db.Column(db.String(255))
 
+    @property
+    def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
     def __repr__(self):
        return f'User {self.username}'
 

e12bae2ee3c809b3133d1be5e1cf7c5f373c596a
DebbieElabonga <debbie.elabonga@student.moringaschool.com>
2021-06-13 15:52:46 +0300

    login,logout and register

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,19 @@
+from app.models import Pitch
 from flask import render_template
 from . import main
+from flask_login import login_required
+from .. import db
+from .forms import UpdateProfile,UploadPitch,CommentsForm
 
 # Views
 @main.route('/')
 def index():
     title = "home of pitches"
 
-    return render_template('index.html', title= title)
+    return render_template('index.html',title= title)
+
+@main.route('/pitch')
+def pitch():
+    pitch = Pitch.query.filter_by().first()
+
+    return render_template('pitch.html', pitch= pitch)

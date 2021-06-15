3aa59095cb66ac20ed23912d6c0bdaa8030d5d71
DebbieElabonga <debbie.elabonga@student.moringaschool.com>
2021-06-15 14:27:38 +0300

    add ons

--- app/auth/views.py
+++ app/auth/views.py
@@ -14,9 +14,11 @@ def login():
         user = User.query.filter_by(email = login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
+
+            flash('Invalid username or Password')    
             return redirect(request.args.get('next') or url_for('main.index'))
 
-        flash('Invalid username or Password')
+       
 
     title = "Pitch login"
     return render_template('auth/login.html',form = login_form,title=title)
